f23cc30af00cbfeead54065be5dc9af9f1e48eb9
samueltexa <woolardsamuels@gmail.com>
2024-10-12 02:39:47 +0300

    add firebase_config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAGi6rFI-jtuE0RG8BgVDTwg1xM1T89Cew",
+    authDomain: "registration-system-ede2e.firebaseapp.com",
+    projectId: "registration-system-ede2e",
+    storageBucket: "registration-system-ede2e.appspot.com",
+    messagingSenderId: "618034098271",
+    appId: "1:618034098271:web:509279dcd6ea5cf60fabea",
+    measurementId: "G-0CY8V1NNWP"
+  };
+
+  export default firebaseConfig;
